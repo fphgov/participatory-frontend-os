a62993f800acb07f59c14322390bd27a7f7a7195
Tamás Kardos <kardos.tamas@budapest.hu>
2021-05-29 00:59:18 +0200

    district selection on map - complete

--- src/component/geodata/layerStyles.js
+++ src/component/geodata/layerStyles.js
@@ -1,6 +1,6 @@
 import { useState, useMemo } from 'react';
 
-const colors = ['#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#42d4f4', '#f032e6', '#bfef45', '#fabed4', '#469990', '#dcbeff', '#9A6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#a9a9a9', '#770CAB', '#D2D5D0', '#F1195E', '#D6B51A', '#769AA7']
+const colors = ['#e6194B', '#3cb44b', '#469990', '#4363d8', '#f58231', '#911eb4', '#42d4f4', '#f032e6', '#bfef45', '#fabed4', '#ffe119', '#dcbeff', '#f81511', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#a9a9a9', '#770CAB', '#D2D5D0', '#81f119', '#D6B51A', '#769AA7']
 
 const opacity = 0.4;
 
@@ -14,23 +14,32 @@ function useLayers() {
 
   const layerStyles = useMemo(() => [
     {
-      id: '2',
+      id: '1',
       type: 'fill',
       paint: {
-        'fill-color': colors[0],
-        'fill-outline-color': colors[0],
-        'fill-opacity': layerOptions == 2 ? 1 : opacity
+        'fill-color': '#19506E',
+        'fill-outline-color': '#19506E',
+        'fill-opacity': layerOptions == 1 ? 1 : opacity
       }
     },
-    {
-      id: '3',
-      type: 'fill',
-      paint: {
-        'fill-color': colors[1],
-        'fill-outline-color': colors[1],
-        'fill-opacity': layerOptions == 3 ? 1 : opacity
-      }
-    },
+    // {
+    //   id: '2',
+    //   type: 'fill',
+    //   paint: {
+    //     'fill-color': colors[0],
+    //     'fill-outline-color': colors[0],
+    //     'fill-opacity': layerOptions == 2 ? 1 : opacity
+    //   }
+    // },
+    // {
+    //   id: '3',
+    //   type: 'fill',
+    //   paint: {
+    //     'fill-color': colors[1],
+    //     'fill-outline-color': colors[1],
+    //     'fill-opacity': layerOptions == 3 ? 1 : opacity
+    //   }
+    // },
     {
       id: '4',
       type: 'fill',
@@ -40,24 +49,24 @@ function useLayers() {
         'fill-opacity': layerOptions == 4 ? 1 : opacity
       }
     },
-    {
-      id: '5',
-      type: 'fill',
-      paint: {
-        'fill-color': colors[3],
-        'fill-outline-color': colors[3],
-        'fill-opacity': layerOptions == 5 ? 1 : opacity
-      }
-    },
-    {
-      id: '6',
-      type: 'fill',
-      paint: {
-        'fill-color': colors[4],
-        'fill-outline-color': colors[4],
-        'fill-opacity': layerOptions == 6 ? 1 : opacity
-      }
-    },
+    // {
+    //   id: '5',
+    //   type: 'fill',
+    //   paint: {
+    //     'fill-color': colors[3],
+    //     'fill-outline-color': colors[3],
+    //     'fill-opacity': layerOptions == 5 ? 1 : opacity
+    //   }
+    // },
+    // {
+    //   id: '6',
+    //   type: 'fill',
+    //   paint: {
+    //     'fill-color': colors[4],
+    //     'fill-outline-color': colors[4],
+    //     'fill-opacity': layerOptions == 6 ? 1 : opacity
+    //   }
+    // },
     {
       id: '7',
       type: 'fill',
@@ -112,15 +121,15 @@ function useLayers() {
         'fill-opacity': layerOptions == 12 ? 1 : opacity
       }
     },
-    {
-      id: '13',
-      type: 'fill',
-      paint: {
-        'fill-color': colors[11],
-        'fill-outline-color': colors[11],
-        'fill-opacity': layerOptions == 13 ? 1 : opacity
-      }
-    },
+    // {
+    //   id: '13',
+    //   type: 'fill',
+    //   paint: {
+    //     'fill-color': colors[11],
+    //     'fill-outline-color': colors[11],
+    //     'fill-opacity': layerOptions == 13 ? 1 : opacity
+    //   }
+    // },
     {
       id: '14',
       type: 'fill',
@@ -139,60 +148,60 @@ function useLayers() {
         'fill-opacity': layerOptions == 15 ? 1 : opacity
       }
     },
-    {
-      id: '16',
-      type: 'fill',
-      paint: {
-        'fill-color': colors[14],
-        'fill-outline-color': colors[14],
-        'fill-opacity': layerOptions == 16 ? 1 : opacity
-      }
-    },
-    {
-      id: '17',
-      type: 'fill',
-      paint: {
-        'fill-color': colors[15],
-        'fill-outline-color': colors[15],
-        'fill-opacity': layerOptions == 17 ? 1 : opacity
-      }
-    },
-    {
-      id: '18',
-      type: 'fill',
-      paint: {
-        'fill-color': colors[16],
-        'fill-outline-color': colors[16],
-        'fill-opacity': layerOptions == 18 ? 1 : opacity
-      }
-    },
-    {
-      id: '19',
-      type: 'fill',
-      paint: {
-        'fill-color': colors[17],
-        'fill-outline-color': colors[17],
-        'fill-opacity': layerOptions == 19 ? 1 : opacity
-      }
-    },
-    {
-      id: '20',
-      type: 'fill',
-      paint: {
-        'fill-color': colors[18],
-        'fill-outline-color': colors[18],
-        'fill-opacity': layerOptions == 20 ? 1 : opacity
-      }
-    },
-    {
-      id: '21',
-      type: 'fill',
-      paint: {
-        'fill-color': colors[19],
-        'fill-outline-color': colors[19],
-        'fill-opacity': layerOptions == 21 ? 1 : opacity
-      }
-    },
+    // {
+    //   id: '16',
+    //   type: 'fill',
+    //   paint: {
+    //     'fill-color': colors[14],
+    //     'fill-outline-color': colors[14],
+    //     'fill-opacity': layerOptions == 16 ? 1 : opacity
+    //   }
+    // },
+    // {
+    //   id: '17',
+    //   type: 'fill',
+    //   paint: {
+    //     'fill-color': colors[15],
+    //     'fill-outline-color': colors[15],
+    //     'fill-opacity': layerOptions == 17 ? 1 : opacity
+    //   }
+    // },
+    // {
+    //   id: '18',
+    //   type: 'fill',
+    //   paint: {
+    //     'fill-color': colors[16],
+    //     'fill-outline-color': colors[16],
+    //     'fill-opacity': layerOptions == 18 ? 1 : opacity
+    //   }
+    // },
+    // {
+    //   id: '19',
+    //   type: 'fill',
+    //   paint: {
+    //     'fill-color': colors[17],
+    //     'fill-outline-color': colors[17],
+    //     'fill-opacity': layerOptions == 19 ? 1 : opacity
+    //   }
+    // },
+    // {
+    //   id: '20',
+    //   type: 'fill',
+    //   paint: {
+    //     'fill-color': colors[18],
+    //     'fill-outline-color': colors[18],
+    //     'fill-opacity': layerOptions == 20 ? 1 : opacity
+    //   }
+    // },
+    // {
+    //   id: '21',
+    //   type: 'fill',
+    //   paint: {
+    //     'fill-color': colors[19],
+    //     'fill-outline-color': colors[19],
+    //     'fill-opacity': layerOptions == 21 ? 1 : opacity
+    //   }
+    // },
     {
       id: '22',
       type: 'fill',
@@ -202,36 +211,27 @@ function useLayers() {
         'fill-opacity': layerOptions == 22 ? 1 : opacity
       }
     },
-    {
-      id: '23',
-      type: 'fill',
-      paint: {
-        'fill-color': colors[21],
-        'fill-outline-color': colors[21],
-        'fill-opacity': layerOptions == 23 ? 1 : opacity
-      }
-    },
+    // {
+    //   id: '23',
+    //   type: 'fill',
+    //   paint: {
+    //     'fill-color': colors[21],
+    //     'fill-outline-color': colors[21],
+    //     'fill-opacity': layerOptions == 23 ? 1 : opacity
+    //   }
+    // },
     {
       id: '24',
       type: 'fill',
       paint: {
         'fill-color': colors[22],
         'fill-outline-color': colors[22],
-        'fill-opacity': layerOptions == 24 ? 1 : opacity
-      }
-    },
-    {
-      id: '25',
-      type: 'fill',
-      paint: {
-        'fill-color': colors[23],
-        'fill-outline-color': colors[23],
-        'fill-opacity': layerOptions == 25 ? 1 : opacity
+        'fill-opacity': layerOptions == 14 ? 0 : layerOptions == 24 ? 1 : opacity
       }
     },
   ], [layerOptions])
 
-  return [layerStyles, layerOptions, handleLayerOptions];
+  return [layerStyles, handleLayerOptions];
 }
 
 export default useLayers;
--- src/component/page/MapBox.js
+++ src/component/page/MapBox.js
@@ -1,13 +1,11 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
-import ReactMapGL, { Marker, Layer, Source } from 'react-map-gl'
+import React, { useState, useEffect } from 'react';
+import ReactMapGL, { Layer, Source } from 'react-map-gl'
 import districtData from '../geodata/districts.json'
 import useLayers from '../geodata/layerStyles'
 
-const districtIndexOnSelect = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
-
 export default function MapBox(props) {
 
-  const [layerStyles, layerOptions, handleLayerOptions] = useLayers();
+  const [layerStyles, handleLayerOptions] = useLayers();
   const [viewport, setViewport] = useState({
     latitude: 47.484,
     longitude: window.innerWidth < 767 ? 19.135 : 19.053,
@@ -21,56 +19,30 @@ export default function MapBox(props) {
     return isDragging ? 'grabbing' : isHovering ? 'pointer' : 'default';
   }
 
-  const districtFilter = useCallback((selectedDistricts, source) => {
-
-    let newGeoJson = JSON.parse(JSON.stringify(source))
-    newGeoJson.features = []
-
-    for (let i = 0; i < selectedDistricts.length; i++) {
-      newGeoJson.features.push(source.features[selectedDistricts[i]])
-    }
-
-    return newGeoJson;
-  }, [])
-
-  // districts to filter, 0-indexed
-  const filteredSourceData = useMemo(() => districtFilter(districtIndexOnSelect, districtData), [])
-
   useEffect(() => {
     handleLayerOptions(props.location)
 
   }, [props.location])
-
-  function convertIndexToId(selected) {
-    return selected.map(elem => {
-      elem = elem + 2;
-      let elemString = `${elem}`
-      return elemString
-    })
-  }
-
-  // console.log(viewport.longitude)
-  // console.log(viewport.latitude)
-  // console.log(viewport.zoom)
 
   return (
     <div className="map-container">
       <ReactMapGL
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        // mapStyle="mapbox://styles/mapbox/streets-v11"
         onViewportChange={(viewport) => { setViewport(viewport) }}
         getCursor={getCursor}
-        interactiveLayerIds={convertIndexToId(districtIndexOnSelect)}
+        interactiveLayerIds={['1', '4', '7', '8', '9', '10', '11', '12', '14', '15', '22', '24']}
         onClick={e => {
           if (e.features[0] && e.features[0].layer.id) {
             props.onChange(e.features[0].layer.id)
+          } else {
+            props.onChange('')
           }
         }}
       >
 
-        {filteredSourceData && filteredSourceData.features.map((district, index) => (
-          <Source key={district.properties.ksh} type="geojson" data={filteredSourceData.features[index]}>
+        {districtData && districtData.features.map((district, index) => (
+          <Source key={district.properties.ksh} type="geojson" data={districtData.features[index]}>
             <Layer {...layerStyles[index]} />
           </Source>
 
