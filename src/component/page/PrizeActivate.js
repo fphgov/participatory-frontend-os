46f92952a98b95d5324429d40fa22f010b0bae95
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2021-07-16 12:39:19 +0200

    Change prize logic

--- src/component/page/PrizeActivate.js
+++ src/component/page/PrizeActivate.js
@@ -4,6 +4,7 @@ import {
 } from "react-router-dom"
 import axios from "../assets/axios"
 import StoreContext from '../../StoreContext'
+import ScrollTo from "../common/ScrollTo"
 
 export default class ProfileActivate extends React.Component {
   static contextType = StoreContext
@@ -12,12 +13,12 @@ export default class ProfileActivate extends React.Component {
     super(props, context)
 
     this.state = {
-      redirectLogin: false
+      redirectLogin: false,
+      error: false,
+      success: '',
     }
 
-    this.context.set('loading', true, () => {
-      this.profileActivate()
-    })
+    this.context.set('loading', false)
   }
 
   toRedirect() {
@@ -27,6 +28,8 @@ export default class ProfileActivate extends React.Component {
   }
 
   profileActivate() {
+    this.context.set('loading', true)
+
     const config = {
       headers: {
         'Accept': 'application/json',
@@ -39,6 +42,12 @@ export default class ProfileActivate extends React.Component {
       .then(response => {
         if (response.data) {
           this.context.set('loading', false)
+        }
+
+        if (response.status === 200) {
+          this.setState({
+            success: response.data.message
+          })
         }
       })
       .catch(error => {
@@ -73,7 +82,7 @@ export default class ProfileActivate extends React.Component {
   }
 
   render() {
-    const { redirectLogin } = this.state
+    const { redirectLogin, error, success } = this.state
 
     if (redirectLogin) {
       return <Redirect to='/bejelentkezes' />
@@ -86,9 +95,20 @@ export default class ProfileActivate extends React.Component {
             <div className="col-md-12">
               <h1>Jelentkezés nyereményjátékra</h1>
 
-              {! this.context.get('loading') ? (<>
-                {this.state.error ? <this.Error message={this.state.error} /> : <this.Success message="Sikeresen jelentkezett a nyereményjátékra!" />}
+              <p>A fővárosi részvételi költségvetésbe szavazatot leadó és a nyereményjátékra jelentkező budapestiek közül kisorsolunk 20 főt, akik a BKV Zrt. jóvoltából ellátogathatnak az 50-es években - a 2-es metróval egyidőben - kiépített, sokáig titokban tartott, a köznyelvben Rákosi-bunkerként ismert atombiztos óvóhelyre.</p>
 
+              <p>A lenti gombra kattintva tud jelentkezni a nyereményjátékra.<br />A jelentkezéssel elfogadja az adatkezelési tájékoztatónk 5/C pontja szerinti, a nyereményjátékhoz <a href="https://otlet.budapest.hu/pb/jsp/site/Portal.jsp?page=htmlpage&htmlpage_id=5" style={{ textDecoration: 'underline' }}>kapcsolódó adatkezelést</a>.</p>
+
+              <p>A nyerteseket augusztus folyamán emailben értesítjük.</p>
+
+              <div className="form-scroll"></div>
+
+              {error ? <ScrollTo element={document.querySelector('.form-scroll').offsetTop}><this.Error message={error} /></ScrollTo> : null}
+              {success ? <this.Success message="Sikeresen jelentkezett a nyereményjátékra!" /> : null}
+
+              {! success ? <input type="submit" value="Jelentkezem" className="btn btn-primary" onClick={this.profileActivate.bind(this)} /> : null}
+
+              {! this.context.get('loading') && success ? (<>
                 <div class="small">
                   <button className="btn btn-primary" onClick={this.toRedirect.bind(this)}>Vissza</button>
                 </div>
