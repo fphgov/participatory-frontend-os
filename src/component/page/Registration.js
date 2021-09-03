397e32ef6f685e97b4ffc7aaa526b1c8f3aa50f0
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2021-09-03 11:27:47 +0200

    Change input order

--- src/component/page/Registration.js
+++ src/component/page/Registration.js
@@ -184,136 +184,136 @@ export default class Registration extends React.Component {
 
                   {! this.state.success ? <>
                     <div className="form-wrapper">
-                    <div className="input-wrapper">
-                      <label htmlFor="email">E-mail <sup>*</sup></label>
-                      <input type="text" placeholder="E-mail" name="email" id="email" value={this.state.email} onChange={this.handleChangeEmailInput} />
+                      <div className="input-wrapper">
+                        <label htmlFor="lastname">Vezetéknév <sup>*</sup></label>
+                        <input type="text" placeholder="Vezetéknév" name="lastname" id="lastname" value={this.state.lastname} onChange={this.handleChangeInput} />
 
-                      {this.state.error && this.state.error.email ? Object.values(this.state.error.email).map((err, i) => {
-                        return <this.ErrorMini key={i} error={err} increment={`email-${i}`} />
+                        {this.state.error && this.state.error.lastname ? Object.values(this.state.error.lastname).map((err, i) => {
+                          return <this.ErrorMini key={i} error={err} increment={`lastname-${i}`} />
+                        }) : null}
+                      </div>
+
+                      <div className="input-wrapper">
+                        <label htmlFor="firstname">Utónév <sup>*</sup></label>
+                        <input type="text" placeholder="Utónév" name="firstname" id="firstname" value={this.state.firstname} onChange={this.handleChangeInput} />
+
+                        {this.state.error && this.state.error.firstname ? Object.values(this.state.error.firstname).map((err, i) => {
+                          return <this.ErrorMini key={i} error={err} increment={`firstname-${i}`} />
+                        }) : null}
+                      </div>
+
+                      <div className="input-wrapper">
+                        <label htmlFor="email">E-mail <sup>*</sup></label>
+                        <input type="text" placeholder="E-mail" name="email" id="email" value={this.state.email} onChange={this.handleChangeEmailInput} />
+
+                        {this.state.error && this.state.error.email ? Object.values(this.state.error.email).map((err, i) => {
+                          return <this.ErrorMini key={i} error={err} increment={`email-${i}`} />
+                        }) : null}
+                      </div>
+
+                      <div className="input-wrapper">
+                        <label htmlFor="password">Jelszó <sup>*</sup></label>
+                        <input type="password" placeholder="Jelszó" name="password" id="password" value={this.state.password} onChange={this.handleChangeRaw} />
+
+                        {this.state.error && this.state.error.password ? Object.values(this.state.error.password).map((err, i) => {
+                          return <this.ErrorMini key={i} error={err} increment={`password-${i}`} />
+                        }) : null}
+                      </div>
+
+                      <div className="input-wrapper">
+                        <label htmlFor="password_confirm">Jelszó megerősítése <sup>*</sup></label>
+                        <input type="password" placeholder="Jelszó megerősítése" name="password_confirm" id="password_confirm" value={this.state.password_confirm} onChange={this.handleChangeRaw} />
+
+                        {this.state.error && this.state.error.password_confirm ? Object.values(this.state.error.password_confirm).map((err, i) => {
+                          return <this.ErrorMini key={i} error={err} increment={`password_confirm-${i}`} />
+                        }) : null}
+                      </div>
+
+                      <div className="input-wrapper">
+                        <label htmlFor="birthyear">Születési év <sup>*</sup></label>
+                        <input type="text" placeholder="Születési év" name="birthyear" id="birthyear" value={this.state.birthyear} onChange={this.handleChangeInput} />
+
+                        {this.state.error && this.state.error.birthyear ? Object.values(this.state.error.birthyear).map((err, i) => {
+                          return <this.ErrorMini key={i} error={err} increment={`birthyear-${i}`} />
+                        }) : null}
+                      </div>
+
+                      <div className="input-wrapper">
+                        <label htmlFor="postal_code">Irányítószám <sup>*</sup></label>
+                        <input type="text" placeholder="Irányítószám" name="postal_code" id="postal_code" value={this.state.postal_code} onChange={this.handleChangeInput} />
+
+                        {this.state.error && this.state.error.postal_code ? Object.values(this.state.error.postal_code).map((err, i) => {
+                          return <this.ErrorMini key={i} error={err} increment={`postal_code-${i}`} />
+                        }) : null}
+                      </div>
+
+                      <h4>Honnan értesült a részvételi költségvetésről? <sup>*</sup></h4>
+                      {this.state.error && this.state.error.hear_about ? Object.values(this.state.error.hear_about).map((err, i) => {
+                        return <this.ErrorMini key={i} error={err} increment={`hear_about-${i}`} />
                       }) : null}
+                      <div className="form-group form-group-hear-about">
+                        <select name="hear_about" onChange={this.handleChangeInput}>
+                          <option value="">Válasszon a lehetőségek közül</option>
+                          <option disabled>---</option>
+                          <option value="friend">Barátoktól, ismerőstől, családtól</option>
+                          <option value="street">Utcai plakátról</option>
+                          <option value="news">Híroldalról, rádióból, TV-ből</option>
+                          <option value="transport">Tömegközlekedési jármű hirdetésből</option>
+                          <option value="districtevent">Kerületi tájékoztató eseményről</option>
+                          <option value="facebook">Facebook bejegyzésből vagy hirdetésből</option>
+                          <option value="civil">Civil szervezet hírleveléből, vagy civil szervezeti találkozón</option>
+                          <option value="library">A Fővárosi Szabó Ervin Könyvtárban található brosúrából</option>
+                          <option value="other">Egyéb</option>
+                        </select>
+                      </div>
+
+                      <div className="form-group">
+                        <label htmlFor="live_in_city" className="form-group-label">
+                          <input className="form-control" type="checkbox" id="live_in_city" name="live_in_city" onChange={this.handleChangeInput} />
+                          Kijelentem, hogy elmúltam 18 éves és budapesti lakos vagyok, vagy Budapesten dolgozom, vagy Budapesten tanulok. *
+                        </label>
+
+                        {this.state.error && this.state.error.live_in_city ? Object.values(this.state.error.live_in_city).map((err, i) => {
+                          return <this.ErrorMini key={i} error={err} increment={`live_in_city-${i}`} />
+                        }) : null}
+                      </div>
+
+                      <div className="form-group">
+                        <label htmlFor="privacy" className="form-group-label">
+                          <input className="form-control" type="checkbox" id="privacy" name="privacy" onChange={this.handleChangeInput} />
+                            Elfogadom az <a href="https://otlet.budapest.hu/pb/jsp/site/Portal.jsp?page=htmlpage&amp;htmlpage_id=5" target="_blank" rel="noopener noreferrer">adatvédelmi tájékoztatót</a>  *
+                        </label>
+
+                        {this.state.error && this.state.error.privacy ? Object.values(this.state.error.privacy).map((err, i) => {
+                          return <this.ErrorMini key={i} error={err} increment={`privacy-${i}`} />
+                        }) : null}
+                      </div>
+
+                      <div className="form-group">
+                        <label htmlFor="prize" className="form-group-label">
+                          <input className="form-control" type="checkbox" id="prize" name="prize" onChange={this.handleChangeInput} />
+                            Részt veszek a <a href="https://otlet.budapest.hu/pb/jsp/site/Portal.jsp?document_id=34&portlet_id=159" target="_blank" rel="noopener noreferrer">Budapest Főpolgármesteri Hivatal nyereményjátékában</a>.
+                        </label>
+
+                        {this.state.error && this.state.error.prize ? Object.values(this.state.error.prize).map((err, i) => {
+                          return <this.ErrorMini key={i} error={err} increment={`prize-${i}`} />
+                        }) : null}
+                      </div>
+
+                      <ReCaptcha
+                        ref={ref => this.recaptcha = ref}
+                        sitekey={process.env.SITE_KEY}
+                        action='submit'
+                        verifyCallback={this.verifyCallback}
+                      />
+
+                      <div style={{ display: "inline-block" }}>
+                        <button className="btn btn-primary">
+                          <span className="glyphicon glyphicon-lock"></span>
+                          Regisztrálok
+                        </button>
+                      </div>
                     </div>
-
-                    <div className="input-wrapper">
-                      <label htmlFor="password">Jelszó <sup>*</sup></label>
-                      <input type="password" placeholder="Jelszó" name="password" id="password" value={this.state.password} onChange={this.handleChangeRaw} />
-
-                      {this.state.error && this.state.error.password ? Object.values(this.state.error.password).map((err, i) => {
-                        return <this.ErrorMini key={i} error={err} increment={`password-${i}`} />
-                      }) : null}
-                    </div>
-
-                    <div className="input-wrapper">
-                      <label htmlFor="password_confirm">Jelszó megerősítése <sup>*</sup></label>
-                      <input type="password" placeholder="Jelszó megerősítése" name="password_confirm" id="password_confirm" value={this.state.password_confirm} onChange={this.handleChangeRaw} />
-
-                      {this.state.error && this.state.error.password_confirm ? Object.values(this.state.error.password_confirm).map((err, i) => {
-                        return <this.ErrorMini key={i} error={err} increment={`password_confirm-${i}`} />
-                      }) : null}
-                    </div>
-
-                    <div className="input-wrapper">
-                      <label htmlFor="lastname">Vezetéknév <sup>*</sup></label>
-                      <input type="text" placeholder="Vezetéknév" name="lastname" id="lastname" value={this.state.lastname} onChange={this.handleChangeInput} />
-
-                      {this.state.error && this.state.error.lastname ? Object.values(this.state.error.lastname).map((err, i) => {
-                        return <this.ErrorMini key={i} error={err} increment={`lastname-${i}`} />
-                      }) : null}
-                    </div>
-
-                    <div className="input-wrapper">
-                      <label htmlFor="firstname">Utónév <sup>*</sup></label>
-                      <input type="text" placeholder="Utónév" name="firstname" id="firstname" value={this.state.firstname} onChange={this.handleChangeInput} />
-
-                      {this.state.error && this.state.error.firstname ? Object.values(this.state.error.firstname).map((err, i) => {
-                        return <this.ErrorMini key={i} error={err} increment={`firstname-${i}`} />
-                      }) : null}
-                    </div>
-
-                    <div className="input-wrapper">
-                      <label htmlFor="birthyear">Születési év <sup>*</sup></label>
-                      <input type="text" placeholder="Születési év" name="birthyear" id="birthyear" value={this.state.birthyear} onChange={this.handleChangeInput} />
-
-                      {this.state.error && this.state.error.birthyear ? Object.values(this.state.error.birthyear).map((err, i) => {
-                        return <this.ErrorMini key={i} error={err} increment={`birthyear-${i}`} />
-                      }) : null}
-                    </div>
-
-                    <div className="input-wrapper">
-                      <label htmlFor="postal_code">Irányítószám <sup>*</sup></label>
-                      <input type="text" placeholder="Irányítószám" name="postal_code" id="postal_code" value={this.state.postal_code} onChange={this.handleChangeInput} />
-
-                      {this.state.error && this.state.error.postal_code ? Object.values(this.state.error.postal_code).map((err, i) => {
-                        return <this.ErrorMini key={i} error={err} increment={`postal_code-${i}`} />
-                      }) : null}
-                    </div>
-
-                    <h4>Honnan értesült a részvételi költségvetésről? <sup>*</sup></h4>
-                    {this.state.error && this.state.error.hear_about ? Object.values(this.state.error.hear_about).map((err, i) => {
-                      return <this.ErrorMini key={i} error={err} increment={`hear_about-${i}`} />
-                    }) : null}
-                    <div className="form-group form-group-hear-about">
-                      <select name="hear_about" onChange={this.handleChangeInput}>
-                        <option value="">Válasszon a lehetőségek közül</option>
-                        <option disabled>---</option>
-                        <option value="friend">Barátoktól, ismerőstől, családtól</option>
-                        <option value="street">Utcai plakátról</option>
-                        <option value="news">Híroldalról, rádióból, TV-ből</option>
-                        <option value="transport">Tömegközlekedési jármű hirdetésből</option>
-                        <option value="districtevent">Kerületi tájékoztató eseményről</option>
-                        <option value="facebook">Facebook bejegyzésből vagy hirdetésből</option>
-                        <option value="civil">Civil szervezet hírleveléből, vagy civil szervezeti találkozón</option>
-                        <option value="library">A Fővárosi Szabó Ervin Könyvtárban található brosúrából</option>
-                        <option value="other">Egyéb</option>
-                      </select>
-                    </div>
-
-                    <div className="form-group">
-                      <label htmlFor="live_in_city" className="form-group-label">
-                        <input className="form-control" type="checkbox" id="live_in_city" name="live_in_city" onChange={this.handleChangeInput} />
-                        Kijelentem, hogy elmúltam 18 éves és budapesti lakos vagyok, vagy Budapesten dolgozom, vagy Budapesten tanulok. *
-                      </label>
-
-                      {this.state.error && this.state.error.live_in_city ? Object.values(this.state.error.live_in_city).map((err, i) => {
-                        return <this.ErrorMini key={i} error={err} increment={`live_in_city-${i}`} />
-                      }) : null}
-                    </div>
-
-                    <div className="form-group">
-                      <label htmlFor="privacy" className="form-group-label">
-                        <input className="form-control" type="checkbox" id="privacy" name="privacy" onChange={this.handleChangeInput} />
-                          Elfogadom az <a href="https://otlet.budapest.hu/pb/jsp/site/Portal.jsp?page=htmlpage&amp;htmlpage_id=5" target="_blank" rel="noopener noreferrer">adatvédelmi tájékoztatót</a>  *
-                      </label>
-
-                      {this.state.error && this.state.error.privacy ? Object.values(this.state.error.privacy).map((err, i) => {
-                        return <this.ErrorMini key={i} error={err} increment={`privacy-${i}`} />
-                      }) : null}
-                    </div>
-
-                    <div className="form-group">
-                      <label htmlFor="prize" className="form-group-label">
-                        <input className="form-control" type="checkbox" id="prize" name="prize" onChange={this.handleChangeInput} />
-                          Részt veszek a <a href="https://otlet.budapest.hu/pb/jsp/site/Portal.jsp?document_id=34&portlet_id=159" target="_blank" rel="noopener noreferrer">Budapest Főpolgármesteri Hivatal nyereményjátékában</a>.
-                      </label>
-
-                      {this.state.error && this.state.error.prize ? Object.values(this.state.error.prize).map((err, i) => {
-                        return <this.ErrorMini key={i} error={err} increment={`prize-${i}`} />
-                      }) : null}
-                    </div>
-
-                    <ReCaptcha
-                      ref={ref => this.recaptcha = ref}
-                      sitekey={process.env.SITE_KEY}
-                      action='submit'
-                      verifyCallback={this.verifyCallback}
-                    />
-
-                    <div style={{ display: "inline-block" }}>
-                      <button className="btn btn-primary">
-                        <span className="glyphicon glyphicon-lock"></span>
-                        Regisztrálok
-                      </button>
-                    </div>
-                  </div>
                   </> : null}
                 </fieldset>
               </form>
