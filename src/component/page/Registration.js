7abb04dcee0d24ce37f8a76e00ce06ea046cb770
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2021-09-23 09:56:43 +0200

    Remove asterisk symbol two field

--- src/component/page/Registration.js
+++ src/component/page/Registration.js
@@ -239,7 +239,7 @@ export default class Registration extends React.Component {
                       </div>
 
                       <div className="input-wrapper">
-                        <label htmlFor="postal_code">Irányítószám <sup>*</sup></label>
+                        <label htmlFor="postal_code">Irányítószám</label>
                         <input type="text" placeholder="Irányítószám" name="postal_code" id="postal_code" value={this.state.postal_code} onChange={this.handleChangeInput} />
 
                         {this.state.error && this.state.error.postal_code ? Object.values(this.state.error.postal_code).map((err, i) => {
@@ -247,7 +247,7 @@ export default class Registration extends React.Component {
                         }) : null}
                       </div>
 
-                      <h4>Honnan értesült a részvételi költségvetésről? <sup>*</sup></h4>
+                      <h4>Honnan értesült a részvételi költségvetésről?</h4>
                       {this.state.error && this.state.error.hear_about ? Object.values(this.state.error.hear_about).map((err, i) => {
                         return <this.ErrorMini key={i} error={err} increment={`hear_about-${i}`} />
                       }) : null}
@@ -286,17 +286,6 @@ export default class Registration extends React.Component {
 
                         {this.state.error && this.state.error.privacy ? Object.values(this.state.error.privacy).map((err, i) => {
                           return <this.ErrorMini key={i} error={err} increment={`privacy-${i}`} />
-                        }) : null}
-                      </div>
-
-                      <div className="form-group">
-                        <label htmlFor="prize" className="form-group-label">
-                          <input className="form-control" type="checkbox" id="prize" name="prize" onChange={this.handleChangeInput} />
-                            Részt veszek a <a href="https://otlet.budapest.hu/pb/jsp/site/Portal.jsp?document_id=34&portlet_id=159" target="_blank" rel="noopener noreferrer">Budapest Főpolgármesteri Hivatal nyereményjátékában</a>.
-                        </label>
-
-                        {this.state.error && this.state.error.prize ? Object.values(this.state.error.prize).map((err, i) => {
-                          return <this.ErrorMini key={i} error={err} increment={`prize-${i}`} />
                         }) : null}
                       </div>
 
