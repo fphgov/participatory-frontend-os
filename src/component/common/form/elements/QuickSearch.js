02eaa6f53ce71e22b43f51fc4921c0da2b2bd121
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2021-10-28 12:34:29 +0200

    Fix field clear

--- src/component/common/form/elements/QuickSearch.js
+++ src/component/common/form/elements/QuickSearch.js
@@ -34,7 +34,6 @@ export default function QuickSearch({ changeRaw, location, error }) {
     }
 
     setLoad(true)
-    changeRaw('location', null)
 
     const searchData = new URLSearchParams(data)
 
