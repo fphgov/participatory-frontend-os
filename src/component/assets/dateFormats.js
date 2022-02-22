242108b7b630367a84de9678cecc3ba4078d1b4d
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2022-02-22 13:21:30 +0100

    Add dot to dateformat

--- src/component/assets/dateFormats.js
+++ src/component/assets/dateFormats.js
@@ -1,7 +1,7 @@
 const getHungarianDateFormat = (rawFormat) => {
   const date = new Date(rawFormat)
 
-  return `${date.getFullYear()}.${("0" + (date.getMonth() + 1)).slice(-2)}.${("0" + date.getDate()).slice(-2)}`
+  return `${date.getFullYear()}.${("0" + (date.getMonth() + 1)).slice(-2)}.${("0" + date.getDate()).slice(-2)}.`
 }
 
 const getDateFormat = (rawFormat) => {
