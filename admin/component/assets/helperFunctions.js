cf601ade4735b817255dc4fb7a32993f5558edbc
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2022-04-08 14:36:21 +0200

    Fix download URL for document links

--- admin/component/assets/helperFunctions.js
+++ admin/component/assets/helperFunctions.js
@@ -28,7 +28,7 @@ export function getDocuments(medias) {
   if (!Array.isArray(medias)) return
 
   return medias.filter(media => documentMimes.indexOf(media.type) > -1).map((item) => {
-    const link = process.env.REACT_APP_API_SERVER + process.env.REACT_APP_API_REQ_MEDIA.toString().replace(':id', item.id)
+    const link = process.env.REACT_APP_API_SERVER + process.env.REACT_APP_API_REQ_MEDIA_DOWNLOAD.toString().replace(':id', item.id)
 
     return { original: link }
   })
