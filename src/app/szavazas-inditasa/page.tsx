6955206dd9252290da3336223b5561f275ec7a87
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2023-08-29 22:31:18 +0200

    Set VoteStartSection if login already
    Signed-off-by: Tamás András Horváth <horvath.tamas.a@budapest.hu>

--- src/app/szavazas-inditasa/page.tsx
+++ src/app/szavazas-inditasa/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import HeroPage from '@/components/common/HeroPage'
 import VoteStartSection from '@/components/vote/VoteStartSection'
+import { apiVoteStatus } from '@/lib/api-requests'
+import { getToken } from '@/lib/actions'
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
@@ -9,12 +11,26 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default async function VoteStartPage() {
+  const token = (await getToken())?.value
+
+  let pageData, error
+
+  if (token) {
+    try {
+      pageData = await apiVoteStatus()
+    } catch (e: any) {
+      error = e.message
+    }
+  }
+
+  const themes = pageData?.data?.projects.map(project => project.campaignTheme?.code as string)
+
   return (
     <main className="page page-vote-start">
       <div className="page-vote-start-section">
         <HeroPage title="Szavazás" content="Itt találod azokat az ötleteket, amikre szavazhatsz. Öt kategória van, minden kategóriában egy szavazatot adhatsz le." />
 
-        <VoteStartSection title="Válassz egy kategóriát és szavazz a kedvenc ötletedre!" />
+        <VoteStartSection title="Válassz egy kategóriát és szavazz a kedvenc ötletedre!" votedList={themes} />
       </div>
     </main>
   )
