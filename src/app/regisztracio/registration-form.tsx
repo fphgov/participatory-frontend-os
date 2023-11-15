2a4c6c2ec59307d6d6246a5f34bb9eacbbd58685
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2023-11-15 12:04:58 +0100

    Add postal_code_type field
    Signed-off-by: Tamás András Horváth <horvath.tamas.a@budapest.hu>

--- src/app/regisztracio/registration-form.tsx
+++ src/app/regisztracio/registration-form.tsx
@@ -7,6 +7,7 @@ import { rmAllCharForEmail, rmAllCharForName, rmForNumber } from "@/utilities/re
 import { useEffect, useState } from "react"
 import ScrollTo from "@/components/common/ScrollTo"
 import { registrationFom } from "@/app/actions"
+import SimpleRadio from "@/components/common/form-element/SimpleRadio"
 
 export default function RegistrationForm(): JSX.Element {
   const [ error, setError ] = useState('')
@@ -22,6 +23,7 @@ export default function RegistrationForm(): JSX.Element {
     'lastname': '',
     'firstname': '',
     'postal_code': '',
+    'postal_code_type': '',
     'birthyear': '',
     'hear_about': '',
     'live_in_city': '',
@@ -60,6 +62,7 @@ export default function RegistrationForm(): JSX.Element {
       lastname: filterData.lastname,
       firstname: filterData.firstname,
       postal_code: filterData.postal_code,
+      postal_code_type: filterData.postal_code_type,
       birthyear: filterData.birthyear,
       hear_about: filterData.hear_about,
       live_in_city: filterData.live_in_city,
@@ -158,11 +161,65 @@ export default function RegistrationForm(): JSX.Element {
 
               <div className="input-wrapper">
                 <label htmlFor="postal_code">Irányítószám *</label>
-                <p className="tipp">Ahol élsz, vagy ha ez nem Budapest, akkor a budapesti munkahelyed/iskolád irányítószáma.</p>
+                <p className="tipp">Lakóhelyed irányítószáma, vagy, ha ez nem budapesti, akkor budapesti munkahelyed/iskolád címéhez tartozó irányítószám.</p>
                 <input type="text" placeholder="Irányítószám" name="postal_code" id="postal_code" maxLength={4} value={filterData.postal_code} onChange={handleChangeNumberInput} />
 
                 {errorObject?.postal_code ? Object.values(errorObject.postal_code).map((err, i) => {
                   return <ErrorMini key={i} error={err} increment={`postal_code-${i}`} />
+                }) : null}
+              </div>
+
+              <div className="input-wrapper">
+                <label htmlFor="postal_code_type">Irányítószám típusa *</label>
+                <p className="tipp">Válassz, hogy a megadott irányítószám mire vonatkozik.</p>
+
+                <div className="form-group">
+                  <div className="input-wrapper card-radio-wrapper">
+                    <div className="row">
+                      <div className="col-md-12">
+                        <div className="card-radio-simple">
+                          <SimpleRadio
+                            id="postal_code_live"
+                            name="postal_code_type"
+                            value="1"
+                            radioValue={filterData.postal_code_type}
+                            title="A megadott irányítószámon élek."
+                            handleChange={handleChangeInput}
+                          />
+                        </div>
+                      </div>
+                      <div className="col-md-12">
+                        <div className="card-radio-simple">
+                          <SimpleRadio
+                            id="postal_code_learn"
+                            name="postal_code_type"
+                            value="2"
+                            radioValue={filterData.postal_code_type}
+                            title="A megadott irányítószámon tanulok."
+                            handleChange={handleChangeInput}
+                          />
+                        </div>
+                      </div>
+                      <div className="col-md-12">
+                        <div className="card-radio-simple">
+                          <SimpleRadio
+                            id="postal_code_work"
+                            name="postal_code_type"
+                            value="3"
+                            radioValue={filterData.postal_code_type}
+                            title="A megadott irányítószámon dolgozok."
+                            handleChange={handleChangeInput}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                {/* <input type="text" placeholder="Irányítószám" name="postal_code_type" id="postal_code_type" maxLength={4} value={filterData.postal_code_type} onChange={handleChangeNumberInput} /> */}
+
+                {errorObject?.postal_code_type ? Object.values(errorObject.postal_code_type).map((err, i) => {
+                  return <ErrorMini key={i} error={err} increment={`postal_code_type-${i}`} />
                 }) : null}
               </div>
 
