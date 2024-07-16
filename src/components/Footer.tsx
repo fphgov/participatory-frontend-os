5982448a232eb575786047bb638f4fa08806c187
Horváth Tamás András <htomy92@gmail.com>
2024-07-16 22:27:32 +0200

    (refact) Footer menu
    Signed-off-by: Horváth Tamás András <htomy92@gmail.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,31 +11,15 @@ export default function Footer(): JSX.Element {
             <div className="logo-wrapper">
               <Image
                 src="/images/category-open-text.svg"
-                width={140}
-                height={64}
+                width={90}
+                height={41}
                 alt="logo"
                 className="logo"
               />
             </div>
 
-            <div className="sharing">
-              <a href="https://www.facebook.com/budapestmindenkie" title="Kövess minket Facebookon" rel="noopener noreferrer">
-              <Image
-                src="/images/social-fb.svg"
-                width={36}
-                height={36}
-                alt="Facebook logo"
-                className="logo"
-                aria-hidden={true}
-              />
-              </a>
-            </div>
-          </div>
-
-          <div className="copyright-wrapper">
-            <div className="copyright">© 2023 Budapest Főváros Önkormányzata | Minden jog fenntartva</div>
-
             <ul>
+              <li className="nav"><Link href="/otletek?campaign=4&rand=6540673">Korábbi ötletek</Link></li>
               <li className="nav"><button type="button" aria-label="Sütikezelés" data-cc="c-settings">Sütikezelés</button></li>
               <li className="nav"><a href={`${process.env.NEXT_PUBLIC_FILES_PATH}/sutitajekoztato.pdf`} target="_blank" rel="noopener noreferrer">Sütitájékoztató</a></li>
               <li className="nav"><a href={`${process.env.NEXT_PUBLIC_FILES_PATH}/adatkezelesi_tajekoztato.pdf`} target="_blank" rel="noopener noreferrer">Adatkezelési tájékoztató</a></li>
@@ -43,6 +27,50 @@ export default function Footer(): JSX.Element {
               <li className="nav"><Link href="/oldal/kapcsolat">Kapcsolat</Link></li>
               <li className="nav"><a href={`${process.env.NEXT_PUBLIC_FILES_PATH}/participatory-budgeting-in-budapest.pdf`} target="_blank" rel="noopener noreferrer">Information in English</a></li>
             </ul>
+          </div>
+
+          <hr />
+
+          <div className="line-two">
+            <div className="logo-secondary-wrapper">
+              <Image
+                src="/images/bp_logo_white.svg"
+                width={201}
+                height={40}
+                alt="logo"
+                className="logo"
+              />
+
+              <div className="sharing sharing-mobile">
+                <a href="https://www.facebook.com/budapestmindenkie" title="Kövess minket Facebookon" rel="noopener noreferrer">
+                <Image
+                  src="/images/social-fb.svg"
+                  width={36}
+                  height={36}
+                  alt="Facebook logo"
+                  className="logo"
+                  aria-hidden={true}
+                />
+                </a>
+              </div>
+            </div>
+
+            <div className="copyright-wrapper">
+              <div className="copyright">© 2024 Budapest Főváros Önkormányzata. Minden jog fenntartva</div>
+
+              <div className="sharing sharing-desktop">
+                <a href="https://www.facebook.com/budapestmindenkie" title="Kövess minket Facebookon" rel="noopener noreferrer">
+                <Image
+                  src="/images/social-fb.svg"
+                  width={36}
+                  height={36}
+                  alt="Facebook logo"
+                  className="logo"
+                  aria-hidden={true}
+                />
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
