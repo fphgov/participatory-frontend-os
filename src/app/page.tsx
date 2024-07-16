1d49e5dd234901536ebf8fdfe14304eeb4ffbf47
Horváth Tamás András <htomy92@gmail.com>
2024-07-17 00:29:45 +0200

    Create new VoteInfoSection and hide other
    Signed-off-by: Horváth Tamás András <htomy92@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Link from 'next/link'
-import TimeLineItem from '@/components/common/TimeLineItem'
-import NewsFeed from '@/components/home/NewsFeed'
-import VoteInfoSection from '@/components/home/VoteInfoSection'
+// import TimeLineWrapper from '@/components/home/TimeLineWrapper'
+// import NewsFeed from '@/components/home/NewsFeed'
+import VoteInfoSectionSec from '@/components/home/VoteInfoSectionSec'
 import NewsletterArea from '@/components/home/NesletterArea'
-import ProjectsFeed from '@/components/home/ProjectsFeed'
+// import ProjectsFeed from '@/components/home/ProjectsFeed'
 
 export default function Home() {
   return (
@@ -14,45 +13,25 @@ export default function Home() {
             <div className="hero-content">
               <div className="container">
                 <div className="row">
-                  <div className="offset-lg-2 col-lg-8">
-                    <h3>Döntsünk közösen! Szavazz szeptember 30-ig!</h3>
+                  <div className="col-12">
+                    <h3>Döntsünk közösen! Szavazz október 10-ig!</h3>
                   </div>
 
-                  <div className="offset-lg-3 col-lg-6">
-                    <p>A közösségi költségvetés keretében idén is budapestiek ötleteire lehet szavazni. A legtöbb szavazatot szerző ötleteket 1 milliárd forint összértékben valósítja meg a Fővárosi Önkormányzat. A KK szabályairól bővebben <Link href="/oldal/bovebben-a-kozossegi-koltsegvetesrol">ITT</Link> olvashatsz.</p>
-
-                    <Link className="btn btn-primary btn-headline btn-next" href="/szavazas-inditasa">Elindítom a szavazást</Link>
+                  <div className="col-12">
+                    <p>A legtöbb szavazatot szerző ötleteket 1 milliárd forint összértékben valósítja meg a Fővárosi Önkormányzat.</p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
 
-          <VoteInfoSection title="A szavazás menete" hasContainerClass={true} />
+          <VoteInfoSectionSec title="A szavazás menete" hasContainerClass={true} />
 
-          <NewsFeed title="Híreink" more="További híreink" />
+          {/* <NewsFeed title="Híreink" more="További híreink" /> */}
 
-          <ProjectsFeed title="Korábbi évek nyertes ötletei" more="További nyertes ötletek" />
+          {/* <ProjectsFeed title="Korábbi évek nyertes ötletei" more="További nyertes ötletek" /> */}
 
-          <div className="light-section fix-section">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-12">
-                  <h3>Közösségi költségvetés menete</h3>
-                </div>
-
-                <div className="col-md-12">
-                  <div className="timeline-wrapper">
-                    <TimeLineItem icon='timeline-icon-1-done.svg' date="2023.11.15. - 2024.01.05." description="Ötletek beküldése" />
-                    <TimeLineItem icon='timeline-icon-2-done.svg' date="2024. első negyedév" description="Szakmai jóváhagyás" />
-                    <TimeLineItem icon='timeline-icon-3.svg' date="2024. tavasz" description="Ötletfejlesztés" />
-                    <TimeLineItem icon='timeline-icon-4.svg' date="2024. ősz" description="Szavazás" />
-                    <TimeLineItem icon='timeline-icon-5.svg' date="2024. ősztől" description="Megvalósítás" />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          {/* <TimeLineWrapper title="Közösségi költségvetés menete" /> */}
         </div>
       </main>
 
---
+++ src/components/home/VoteInfoSectionSec.tsx
@@ -0,0 +1,80 @@
+import React from "react"
+import Link from "next/link"
+
+export type VoteInfoSectionSecProps = {
+  title?: string
+  hasContainerClass?: boolean
+}
+
+export default function VoteInfoSectionSec({ title, hasContainerClass = false }: VoteInfoSectionSecProps): JSX.Element {
+  const content = (
+    <div className="row">
+      <div className="col-12">
+        {title ? <h2>{title}</h2> : null}
+
+        <div className="info-box-wrapper">
+          <div className="info-box">
+            <div className="info-box-num">1</div>
+
+            <div className="info-box-text">
+              <div className="info-box-title">
+                Válassz kategóriát, majd ötletet
+              </div>
+              <div className="info-box-content">
+              5 kategóriában szavazhatsz az ötletekre.
+              </div>
+            </div>
+          </div>
+
+          <div className="info-box">
+            <div className="info-box-num">2</div>
+
+            <div className="info-box-text">
+              <div className="info-box-title">
+                Add le a szavazatod
+              </div>
+              <div className="info-box-content">
+                Minden kategóriában 3 ötletre szavazhatsz.
+              </div>
+            </div>
+          </div>
+
+          <div className="info-box">
+            <div className="info-box-num">3</div>
+
+            <div className="info-box-text">
+              <div className="info-box-title">
+                Lépj be vagy regisztrálj
+              </div>
+              <div className="info-box-content">
+                Elég egy e-mail címet megadnod a szavazáshoz.
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="info-box-more">
+          <Link className="btn btn-primary btn-headline btn-next" href="/szavazas-inditasa">Elindítom a szavazást</Link>
+          <Link className="link-info" href="/szavazas-inditasa">Szavazás feltételei</Link>
+        </div>
+
+      </div>
+    </div>
+  )
+
+  if (! hasContainerClass) {
+    return content
+  }
+
+  return (
+    <div className="info-section-sec-wrapper row">
+      <div className="info-section-sec-content offset-1 col-10">
+        <div className="info-section-sec">
+          <div className="container">
+            {content}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
