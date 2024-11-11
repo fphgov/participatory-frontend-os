d6265f9a3067327041c48ea094f27eba8d0735ae
Robert Binder <binder.robert92@gmail.com>
2024-11-11 18:47:16 +0100

    add name to profile

--- src/components/profile/ProfileBox.tsx
+++ src/components/profile/ProfileBox.tsx
@@ -8,6 +8,10 @@ export default function ProfileBox({ profile }: ProfileBoxProps): JSX.Element {
   return (
     <div className="box-profile">
       <div className="profile-item">
+        <div className="profile-item-name">Név:</div>
+        <div className="profile-item-value">{profile.firstname} {profile.lastname}</div>
+      </div>
+      <div className="profile-item">
         <div className="profile-item-name">E-mail cím:</div>
         <div className="profile-item-value">{profile.email}</div>
       </div>
