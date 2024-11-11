28d854fe24b4e3d81489e11e49db6f492b5cce09
Robert Binder <binder.robert92@gmail.com>
2024-11-11 18:35:51 +0100

    idea form

--- src/components/common/form-element/Select.tsx
+++ src/components/common/form-element/Select.tsx
@@ -8,7 +8,7 @@ type SelectOption = {
 type SelectProps = {
   id?: string
   name: string
-  value: string
+  value: any
   radioValue?: string|boolean|number
   title: string
   tipp?: string
@@ -16,16 +16,17 @@ type SelectProps = {
   showLabel?: boolean
   dataList: SelectOption[]
   handleChange: (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => void
+  multiple?: boolean
 }
 
-export default function Select({ id, name, value = '', title, tipp, defaultValue = '0', showLabel = true, dataList, handleChange }: SelectProps) {
+export default function Select({ id, name, value = '', title, tipp, defaultValue = '0', showLabel = true, dataList, handleChange, multiple = false }: SelectProps) {
   return (
     <div className="input-wrapper">
       {showLabel ? <label htmlFor={name}>{title}</label> : null}
 
-      <div className="tipp">{tipp}</div>
+      {tipp && <div className="tipp">{tipp}</div>}
 
-      <select className={(value.length === 0) ? "" : "valid"} defaultValue={defaultValue} name={name} id={id} value={value} onChange={handleChange}>
+      <select multiple={multiple} className={(value.length === 0) ? "" : "valid"} defaultValue={defaultValue} name={name} id={id} value={value} onChange={handleChange}>
         {dataList.map((option, i) => {
           return (
             <option key={i} value={option.value}>{option.name}</option>
