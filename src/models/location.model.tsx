8c688b08aad7b631a4d02a7161817ef98bdae119
Robert Binder <binder.robert92@gmail.com>
2024-11-14 17:56:43 +0100

    18738

--- src/models/location.model.tsx
+++ src/models/location.model.tsx
@@ -1,5 +1,5 @@
 export type ILocationValue = '0' | '1' | '2'
-export type ILocationName = 'Válassz egy kerületet' | 'Konkrét helyszínhez kötődik' | 'Nem kötődik konkrét helyszínhez'
+export type ILocationName = 'Válassz az alábbi két opció közül!' | 'Konkrét helyszínhez kötődik' | 'Nem kötődik konkrét helyszínhez'
 
 export type ILocation = {
   name: ILocationName
@@ -7,7 +7,7 @@ export type ILocation = {
 }
 
 export const locationDataList: ILocation[] = [
-  { name: 'Válassz egy kerületet', value: '0'},
+  { name: 'Válassz az alábbi két opció közül!', value: '0'},
   { name: 'Konkrét helyszínhez kötődik', value: '1'},
   { name: 'Nem kötődik konkrét helyszínhez', value: '2'},
 ]
